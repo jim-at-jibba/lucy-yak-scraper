200aad9c0cd405232927853262df54b6a4f4d94d
James Best <jim@justjibba.net>
2022-12-07 21:12:08 +0000

    Add simple scraper that will send sms when new pjs are available

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gocolly/colly"
+	twilio "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+	fmt.Println("Scraping")
+	Scrape()
+}
+
+// https://lucyandyak.com/collections/sleepwear
+func Scrape() {
+
+	fmt.Println("We are gonna scrape")
+	c := colly.NewCollector(
+		colly.AllowedDomains("lucyandyak.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.StatusCode)
+	})
+
+	c.OnHTML("#product-grid", func(e *colly.HTMLElement) {
+		var list []string
+		var currentPJCount = 6 // as of 07/12/22
+		fmt.Println("Product list found")
+		e.ForEach("li", func(i int, h *colly.HTMLElement) {
+			list = append(list, h.ChildText(".card__heading"))
+		})
+
+		fmt.Printf("Count %d", len(list))
+		if len(list) > currentPJCount {
+			SendMsg("New PJs available")
+		}
+	})
+
+	c.Visit("https://lucyandyak.com/collections/sleepwear")
+}
+
+func SendMsg(msg string) {
+	client := twilio.NewRestClient()
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(os.Getenv("TO_PHONE_NUMBER"))
+	params.SetFrom(os.Getenv("TWILIO_PHONE_NUMBER"))
+	params.SetBody(msg)
+
+	_, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("SMS sent successfully!")
+	}
+}
